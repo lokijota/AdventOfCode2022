e821b8a1f112ed5bf4b6f9306c3d6c7cd3708169
João Pedro Martins <joao.pedro.martins@gmail.com>
2022-12-21 19:29:42 +0000

    more skelleton code for the game loop. wonder if this is all too inneficient and I should just use a large array

--- Challenges/Exc17/code.py
+++ Challenges/Exc17/code.py
@@ -38,16 +38,46 @@ piece_characteristics["o"] = (2, 2, "1111")
 print(piece_characteristics)
 
 class Board:
-    def __init__(self):
-        self.board = []
+    def __init__(self, gas_generator):
+        self.board = ["1111111"] #represent the bottom -- to make tests simpler? Do I also represent the walls? that's silly, however
+        self.gas_generator = gas_generator
     
     def play_move(self, piece):
+
+        # prepare for the new move - make 3 rows of space
+        self.board.insert(0, "0000000")
+        self.board.insert(0, "0000000")
+        self.board.insert(0, "0000000")
+
+        # calculate starting pos of piece
+        piece_row = self.height() + piece_characteristics[piece][1]
+        piece_col = 2
+
+        moved = True
+        while moved:
+            # the following may return the same position as before, if movement wasn't possible
+            piece_row, piece_col, moved_side = self.MoveSideways(gas_generator.next(), piece, piece_row, piece_col)
+            piece_row, piece_col, moved_down = self.MoveDown(piece, piece_row, piece_col)
+
+            moved = moved_side or moved_down
+
         return piece
     
+    def MoveSideways(self, direction, piece, row, col):
+        return 0, 0, False
+    
+    def MoveDown(self, piece, row, col):
+        return 0, 0, False 
+    
     def height(self):
-        return len(self.board)
+        # this has to be smarter - there may be empty rows at the top after the movement / unless I delete them
+        return len(self.board)-1
 
-b = Board()
+    def overlap(self, piece, row, col):
+        # TBD
+        return False
+
+b = Board(gas_generator)
 
 for i in range(2022):
     b.play_move(piece_generator.next())
