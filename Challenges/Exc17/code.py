01bba2b79e8510ef0048cbb716225ce2e30ac365
João Pedro Martins <joao.pedro.martins@gmail.com>
2022-12-22 19:30:06 +0000

    Exc 17 part 2 -- Mg found the repeating pattern, as in a previous exercise, and this lets you calculate the result by simple math

--- Challenges/Exc17/code.py
+++ Challenges/Exc17/code.py
@@ -5,6 +5,7 @@ import sys
 import os
 import math
 from timeit import default_timer as timer
+from tqdm import tqdm
 
 sys.path.append(os.path.abspath("."))
 print(os.path.abspath("."))
@@ -46,17 +47,30 @@ class Board:
     def play_move(self, piece):
 
         # prepare for the new move - make 3 rows of space + height of the piece
-        for i in range(3 + piece_characteristics[piece][1]):
-            self.board.insert(0, "0000000")
+        count = 0
+        if self.height() >= 1:
+            for i in range(7):
+                if self.board[i] == "0000000":
+                    count += 1
+                else:
+                    break
+        
+        if 3 + piece_characteristics[piece][1]-count > 0:
+            for i in range(3 + piece_characteristics[piece][1]-count):
+                self.board.insert(0, "0000000")
+        else: # sigh. for cases with too many rows on the top... can happen if - comes after |
+            rows_to_remove = -(3 + piece_characteristics[piece][1]-count)
+            for i in range(rows_to_remove):
+                self.board.pop(0)
 
         # calculate starting pos of piece
-        piece_row = 0 # self.height()
+        piece_row = 0
         piece_col = 2
 
         moved_down = True
         while moved_down:
             # the following may return the same position as before, if movement wasn't possible
-            piece_row, piece_col, moved_side = self.MoveSideways(gas_generator.next(), piece, piece_row, piece_col)
+            piece_row, piece_col, _ = self.MoveSideways(gas_generator.next(), piece, piece_row, piece_col)
             piece_row, piece_col, moved_down = self.MoveDown(piece, piece_row, piece_col)
 
         # Now, put the piece in place on the board
@@ -70,9 +84,9 @@ class Board:
 
             # self.board[c[0]][c[1]] = piece
 
-        # remove empty rows from the top -- this can be optimized/avoided... NOTAJOTA
-        while self.board[0] == "0000000":
-            self.board.pop(0)
+        # remove empty rows from the top -- this can be optimized/avoided...
+        # while self.board[0] == "0000000":
+        #     self.board.pop(0)
 
         return piece
     
@@ -111,6 +125,19 @@ class Board:
         # this has to be smarter - there may be empty rows at the top after the movement / unless I delete them
         return len(self.board)
 
+    def true_height(self):
+        
+        # count empty rows at top just with 0000000
+        count = 0
+        if self.height() > 0:
+            for i in range(7):
+                if self.board[i] == "0000000":
+                    count += 1
+                else:
+                    break
+
+        return self.height() - count
+
     def overlap(self, piece, row, col):
         # check if the piece overlaps with existing pieces on the board
         
@@ -131,11 +158,46 @@ class Board:
 
 b = Board(gas_generator)
 
-for i in range(2022):
+"""
+for i in range(2022): 
     b.play_move(piece_generator.next())
     # b.print_board()
     # print()
 
-print(b.height())
-
+print(b.true_height())
 # part 1 - 3159
+exit()
+"""
+
+
+STABILIZE_DELTA = 200
+CYCLE_LENGHT = 1690 # the results start repeating in blocks of 1690 rows (in terms of height increases)
+for i in tqdm(range(STABILIZE_DELTA)): 
+    b.play_move(piece_generator.next())
+
+running_height = b.true_height()
+
+heights = [0]
+
+for i in tqdm(range(CYCLE_LENGHT)):
+    b.play_move(piece_generator.next())
+    heights += [b.true_height() - running_height - sum(heights)] 
+
+heights.pop(0)
+
+print("Sum of first 200 is", running_height)
+print("Sum of each 1670 is", sum(heights))
+print("How many times 1670 fits into the 1B - STABILIZE_DELTA is", math.floor((1000000000000-STABILIZE_DELTA)/CYCLE_LENGHT))
+running_height += math.floor((1000000000000-STABILIZE_DELTA)/CYCLE_LENGHT) * sum(heights)
+
+print("# records processed", STABILIZE_DELTA + math.floor((1000000000000-STABILIZE_DELTA)/CYCLE_LENGHT)*CYCLE_LENGHT)
+
+records_to_process = 1000000000000 - STABILIZE_DELTA - math.floor((1000000000000-STABILIZE_DELTA)/CYCLE_LENGHT)*CYCLE_LENGHT
+print("Records to add", records_to_process)
+
+running_height += sum(heights[:records_to_process])
+
+print("Estimated submission is", running_height)
+
+# part 2 - 1566272189352
+
