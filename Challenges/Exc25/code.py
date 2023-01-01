ff05e3f19cb2c4b9d4c33b142801ec61c2555c71
João Pedro Martins <joao.pedro.martins@gmail.com>
2023-01-01 14:03:52 +0000

    Finally! (exc25 done)

--- Challenges/Exc25/code.py
+++ Challenges/Exc25/code.py
@@ -15,5 +15,57 @@ print(os.path.abspath("."))
 from telegram import *
 
 # ler todas as linhas, como de costume
-with open('Challenges\Exc23\input.txt') as f:
+with open('Challenges\Exc25\input.txt') as f:
     lines = f.read().splitlines()
+
+snafu_base10_converter = {
+    '2': 2,
+    '1': 1,
+    '0': 0,
+    '-': -1,
+    '=': -2
+}
+
+base10_snafu_converter = {
+    4: '2',
+    3: '1',
+    2: '0',
+    1: '-',
+    0: '=',
+}
+
+# add in decimal
+acc = 0
+for snafu_number in lines:
+    exp = 0
+    sum = 0
+    for snafu_digit in snafu_number[::-1]:
+        sum += snafu_base10_converter[snafu_digit] * (5 ** exp)
+        exp += 1
+    acc += sum
+    # print(sum)
+print(acc)
+
+# convert back to snafu
+snafu_number = ''
+
+while acc > 0:
+    snafu_number = base10_snafu_converter[(acc+2)% 5] + snafu_number
+    acc = (acc+2) // 5
+
+print(snafu_number)
+
+"""
+     0        0
+     1        1
+     2        2
+    1=        3 
+    1-        4
+    10        5
+    11        6
+    12        7
+    2=        8
+    2-        9
+    20       10
+    21       11
+"""
